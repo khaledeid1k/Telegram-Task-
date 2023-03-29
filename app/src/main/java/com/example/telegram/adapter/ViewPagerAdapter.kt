af1e9418a290333c703b267b3d64f5e55cd6015b
khaled eid <khaled.eid1k1k@gmail.com>
2023-03-30 00:33:36 +0200

    optimize adapter of viewPager

--- app/src/main/java/com/example/telegram/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/telegram/adapter/ViewPagerAdapter.kt
@@ -1,31 +1,18 @@
 package com.example.telegram.adapter
 
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.lifecycle.Lifecycle
+import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.telegram.ui.Files
-import com.example.telegram.ui.Links
 import com.example.telegram.ui.Media
 import com.example.telegram.ui.Music
 
 
-private const val NUM_TABS = 4
 
-class ViewPagerAdapter(fragmentManager: FragmentManager,lifecycle: Lifecycle)
-    :FragmentStateAdapter(fragmentManager,lifecycle) {
+class ViewPagerAdapter(
+    container: FragmentActivity,
+    private val fragmentList: Array<Fragment>
 
-    override fun getItemCount(): Int {
-        return NUM_TABS
-    }
-
-    override fun createFragment(position: Int): Fragment {
-        when (position) {
-            0 -> return Media()
-            1 -> return Files()
-            2 -> return Links()
-        }
-        return Music()
-
-    }
+) : FragmentStateAdapter(container) {
+    override fun getItemCount()= fragmentList.size
+    override fun createFragment(position: Int)=fragmentList[position]
 }
--- app/src/main/java/com/example/telegram/ui/MainActivity.kt
+++ app/src/main/java/com/example/telegram/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.telegram.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
 import androidx.viewpager2.widget.ViewPager2
 import com.example.telegram.R
 import com.example.telegram.adapter.ViewPagerAdapter
@@ -16,14 +17,32 @@ class MainActivity : AppCompatActivity() {
         "Links",
         "Musics"
     )
+    private val fragmentList = arrayOf(
+        Media(),
+        Files(),
+        Links(),
+        Music()
+    )
+ private lateinit  var viewPager : ViewPager2
+ private lateinit  var tabLayout : TabLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        val viewPager= findViewById<ViewPager2>(R.id.viewPager)
-        val tabLayout=findViewById<TabLayout>(R.id.tabLayout)
-        val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+         viewPager= findViewById(R.id.viewPager)
+         tabLayout=findViewById<TabLayout>(R.id.tabLayout)
+
+        initViewPager()
+        initTabLayout()
+
+    }
+
+    private fun initViewPager(){
+        val adapter = ViewPagerAdapter(this, fragmentList)
         viewPager.adapter=adapter
+    }
+
+    private fun initTabLayout(){
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = dataArray[position]
         }.attach()
